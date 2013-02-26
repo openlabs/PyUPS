9f2c882a5c0c57b6c5668278d4f01514e8bbe10b
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2013-02-26 12:48:36 +0530

    Refactor setup.py to avoid loading package to find version

--- ups/__init__.py
+++ ups/__init__.py
@@ -3,13 +3,9 @@
 
     XML Based API to UPS
 
-    :copyright: (c) 2010-2011 by Openlabs Technologies & Consulting (P) LTD
+    :copyright: (c) 2010-2013 by Openlabs Technologies & Consulting (P) LTD
     :copyright: (c) 2010 by Sharoon Thomas.
     :license: AGPL, see LICENSE for more details
 '''
-__author__ = 'Openlabs Technologies & Consulting (P) LTD'
-__version__ = '0.4.3'
-
-
 from .shipping_package import ShipmentConfirm, ShipmentAccept, ShipmentVoid
 from .shipping_package import PyUPSException
