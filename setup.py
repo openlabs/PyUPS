b436b9cfe8a7771104256d2c62281090718d256a
Tarun Bhardwaj <tarun.bhardwaj@openlabs.co.in>
2014-10-29 12:29:53 +0530

    Release 0.6.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup
 
 setup(
     name='PyUPS',
-    version='0.6.0',
+    version='0.6.1',
     license='AGPL',
     author='Openlabs Technologies & Consulting (P) LTD',
     author_email='info@openlabs.co.in',
